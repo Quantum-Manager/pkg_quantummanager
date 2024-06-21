5c93fb2b226162d053156bafc225de2d748f3d01
dmitriitsymbal <cymbal@delo-design.ru>
2024-06-21 21:14:42 +0300

    поправил скрипт установки

--- script.php
+++ script.php
@@ -72,41 +72,9 @@ class pkg_QuantummanagerInstallerScript
 				return false;
 			}
 
-			//Download remotes
-			$this->downloadRemotes($parent);
 		}
 
 	}
-
-
-	public function postflight($type, $parent)
-	{
-		if ($type === 'update' || $type === 'install')
-		{
-			$msg    = '';
-			$result = $this->installLibFields($parent);
-
-			if ($result !== true)
-			{
-				$msg .= Text::sprintf('PKG_QUANTUMMANAGER_LIBFIELDS_INSTALLATION_ERROR', (string) $result);
-			}
-
-			if ($msg)
-			{
-				Factory::getApplication()->enqueueMessage($msg, 'error');
-
-				return false;
-			}
-
-			if ($type === 'update')
-			{
-				$this->update150();
-			}
-
-		}
-
-	}
-
 
 	/**
 	 * Method to check compatible.
@@ -166,124 +134,6 @@ class pkg_QuantummanagerInstallerScript
 
 			return false;
 		}
-
-		return true;
-	}
-
-	/**
-	 * Method to download remotes.
-	 *
-	 * @param   InstallerAdapter  $parent  Parent object calling object.
-	 *
-	 * @throws  Exception
-	 *
-	 * @since  __DEPLOY_VERSION__
-	 */
-	protected function downloadRemotes($parent)
-	{
-		$attributes = $parent->getParent()->manifest->xpath('files');
-		$source     = $parent->getParent()->getPath('source');
-
-		if (!is_array($attributes) || empty($attributes[0]))
-		{
-			return;
-		}
-
-		foreach ($attributes[0] as $type => $value)
-		{
-			if (!empty($value->attributes()->download) && !empty($value[0]))
-			{
-				$src  = htmlspecialchars_decode((string) $value->attributes()->download);
-				$dest = $source . '/' . $value[0];
-				@file_put_contents($dest, file_get_contents($src));
-			}
-		}
-	}
-
-	protected function update150()
-	{
-		$db        = Factory::getDBO();
-		$query     = $db->getQuery(true)
-			->select($db->quoteName(['extension_id']))
-			->from('#__extensions')
-			->where('element =' . $db->quote('quantummanagercontent'))
-			->where('folder =' . $db->quote('editors-xtd'));
-		$extension = $db->setQuery($query)->loadObject();
-
-		if (isset($extension->extension_id) && ((int) $extension->extension_id > 0))
-		{
-			$installer = Installer::getInstance();
-			$installer->uninstall('plugin', (int) $extension->extension_id);
-		}
-
-		$db        = Factory::getDBO();
-		$query     = $db->getQuery(true)
-			->select($db->quoteName(['extension_id']))
-			->from('#__extensions')
-			->where('element =' . $db->quote('quantummanagercommedia'))
-			->where('folder =' . $db->quote('system'));
-		$extension = $db->setQuery($query)->loadObject();
-
-		if (isset($extension->extension_id) && ((int) $extension->extension_id > 0))
-		{
-			$installer = Installer::getInstance();
-			$installer->uninstall('plugin', (int) $extension->extension_id);
-		}
-
-	}
-
-	protected function installLibFields($parent)
-	{
-
-		$tmp           = Factory::getConfig()->get('tmp_path');
-		$libFieldsFile = 'https://github.com/JPathRu/lib_fields/releases/latest/download/lib_fields.zip';
-		$tmpFile       = Path::clean($tmp . '/lib_fields.zip');
-		$extDir        = Path::clean($tmp . '/' . uniqid('install_'));
-
-		$contents = file_get_contents($libFieldsFile);
-		if ($contents === false)
-		{
-			return Text::sprintf('PKG_QUANTUMMANAGER_LIBFIELDS_IE_FAILED_DOWNLOAD', $libFieldsFile);
-		}
-
-		$resultContents = file_put_contents($tmpFile, $contents);
-		if ($resultContents == false)
-		{
-			return Text::sprintf('PKG_QUANTUMMANAGER_LIBFIELDS_IE_FAILED_INSTALLATION', $tmpFile);
-		}
-
-		if (!file_exists($tmpFile))
-		{
-			return Text::sprintf('PKG_QUANTUMMANAGER_LIBFIELDS_IE_NOT_EXISTS', $tmpFile);
-		}
-
-		$archive = new Archive(['tmp_path' => $tmp]);
-		try
-		{
-			$archive->extract($tmpFile, $extDir);
-		}
-		catch (\Exception $e)
-		{
-			return Text::sprintf('PKG_QUANTUMMANAGER_LIBFIELDS_IE_FAILER_UNZIP', $tmpFile, $extDir, $e->getMesage());
-		}
-
-		$installer = new Installer();
-		$installer->setPath('source', $extDir);
-		if (!$installer->findManifest())
-		{
-			InstallerHelper::cleanupInstall($tmpFile, $extDir);
-
-			return Text::_('PKG_QUANTUMMANAGER_LIBFIELDS_IE_INCORRECT_MANIFEST');
-		}
-
-		if (!$installer->install($extDir))
-		{
-			InstallerHelper::cleanupInstall($tmpFile, $extDir);
-
-			return Text::_('PKG_QUANTUMMANAGER_LIBFIELDS_IE_INSTALLER_ERROR');
-		}
-
-		InstallerHelper::cleanupInstall($tmpFile, $extDir);
 
 		return true;
 	}
